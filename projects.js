29e2ceed74a00322987bf2e596cb69cac00d7288
HBiTiO <mbkdodos@gmail.com>
2026-09-21 03:39:32 +0200

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,19 @@
+const PROJECTS = [
+  {
+    id: "redwood",
+    title: "RedWood",
+    category: "Maps",
+    description: "RedWood est une map Garry's Mod créée totalement par mes soins.",
+    image: "redwood-1.jpg",
+    gallery: [
+      "redwood-1.jpg",
+      "redwood-2.jpg",
+      "redwood-3.jpg",
+      "redwood-4.jpg"
+    ],
+    tags: ["Garry's Mod", "Map", "Création originale"],
+    price: "59,90 €",
+    paymentUrl: "https://buy.stripe.com/28E3cxdgbdPMe0x8x65gc00",
+    status: "Disponible"
+  }
+];
